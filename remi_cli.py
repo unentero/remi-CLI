ead7c78c24d1afe81796d0e130d6824022a059aa
unentero <emmocampo1010@gmail.com>
2026-05-05 21:58:49 -0300

    Added file parameter, removed file input in runtime

--- remi_cli.py
+++ remi_cli.py
@@ -1,8 +1,12 @@
 from difflib import SequenceMatcher
 import click
+from pathlib import Path
+from datetime import datetime
+import os
 # esta parte del codigo se va a encargar de ingresar y procesar el pdf/word de preguntas
 
-
+def clearscr():
+    os.system('cls' if os.name == 'nt' else 'clear')
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
@@ -10,10 +14,7 @@ def dbFilepath(filepath):
     db = filepath[0:len(filepath)-4] + "db.txt"
     return db
 
-
 def agregar_registro(filepath,question,score):
-    from pathlib import Path
-    from datetime import datetime
     filepath = Path(filepath)
     if filepath.is_file() or filepath.is_dir():
         file = open(filepath,"a")
@@ -32,17 +33,16 @@ def agregar_registro(filepath,question,score):
         file.close()
 
 @click.command()
-
-def test():
-    fname = input('Ingresar Archivo: ')
+@click.argument('filename')
+def test(filename):
     try:
-        fhand = open(fname,'r')
+        fhand = open(filename,'r')
     except:
-        click.echo('El archivo no pudo ser encontrado: '+fname)
+        click.echo('El archivo no pudo ser encontrado: '+filename)
         exit()
     linelist = []
     for line in fhand:
-        linelist.append(line[3:].rstrip())
+        linelist.append(line.rstrip())
     questions = [] #aca se almacenan las preguntas y las respuestas
     for i in range(0,len(linelist),2):
         questions.append((linelist[i],linelist[i+1]))
@@ -57,6 +57,6 @@ def test():
         alpha = similar(user_answer, base_answer) # aca se determina la similitud de los strings
         score = alpha*100
         click.echo('Similitud:' + str(score))
-        agregar_registro(dbFilepath(fname),question[0],str(score))
+        agregar_registro(dbFilepath(filename),question[0],str(score))
     
 
